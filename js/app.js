ff781481fa3fb82f31f1c513bb9a6a411fcb9a5a
kisiaradev <fopala@cytonn.com>
2017-02-19 12:01:17 +0300

    Webpack

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.write('Welcome to my app');
+console.log('app loaded');
+
+document.write("<br>I have been changed while being watched")
+document.writeln("<br>Lets see if this will update using webpack development server")
+document.writeln("<br>HAHAHAHA ... It did")
